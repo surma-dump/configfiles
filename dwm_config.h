8e91c12f0b024d107aa1120cb3d9e0991c43bab5
surma <surma@petrelli.search.b.superkabel.de>
2010-01-14 11:26:17 +0100

    added dwm window masks @config.h

--- dwm_config.h
+++ dwm_config.h
@@ -20,6 +20,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "surf",  NULL,       NULL,       1 << 0,       False,       -1 },
+	{ "tabbed",  NULL,       NULL,       1 << 0,       False,       -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 0,       False,       -1 },
 	{ "Pidgin",  NULL,       NULL,       1 << 1,       False,       -1 },
 	{ "Skype",  NULL,       NULL,       1 << 1,       False,       -1 },
 };
